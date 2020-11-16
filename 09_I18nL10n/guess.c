d3e4c611ae417b34c9b9580681ca889e98d03964
max101101 <suhov85@mail.ru>
2020-11-16 22:02:13 +0300

    add 09_I18nL10n

---
+++ 09_I18nL10n/guess.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define _(STRING) gettext(STRING)
+
+int main() {
+	setlocale(LC_ALL, "");
+	bindtextdomain("guess", ".");
+	textdomain("guess");
+
+	int start = 1;
+	int end = 100;
+	printf("%s\n", _("Guess a number in range 1-100"));
+
+	while(1) {
+		if(end - start < 2) {
+			printf("%s: %d\n", _("You Guessed"), end);
+			break;
+		}	
+
+		printf("%s: %d? (%s|%s)\n", _("Is your number bigger than"), start + (end-start)/2, _("yes"), _("no"));
+
+		char ans[32];
+		scanf("%s", ans);
+		if (!strcmp(ans, _("yes"))) {
+			start = start + (end-start)/2;
+			continue;
+		}
+
+		if (!strcmp(ans, _("no"))) {
+			end = start + (end-start)/2;
+			continue;
+		}	
+	}
+
+	return 0;
+}
